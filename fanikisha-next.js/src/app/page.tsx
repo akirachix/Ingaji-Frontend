5b47dc93d58e97c84196d0934f91d1b8bfaf37dd
ingabirefanny <fannyingabiree@gmail.com>
2024-09-26 20:02:00 +0300

    Created overview

--- fanikisha-next.js/src/app/page.tsx
+++ fanikisha-next.js/src/app/page.tsx
@@ -1,25 +1,12 @@
 import { defined } from 'chart.js/helpers';
 import Layout from './components/Layout';
-// import Sidebar from './components/Sidebar';
-// import ChangePassword from './components/ChangePassword';
 import Overview from './components/Overview';
-// import MilkRecords from './components/MilkRecords';
-// import LoadingPage from './components/LoadingPage';
-// import Accounts from './components/Accounts';
-
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
       <Layout>
         <div className="flex">
           <main className="flex-1">{children}</main>
-          {/* <MilkRecords/> */}
           <Overview/>
-          {/* <Accounts/> */}
-          {/* <LoadingPage isLoading={defined}/> */}
-          {/* <ChangePassword/> */}
-
-
         </div>
         </Layout>
       
