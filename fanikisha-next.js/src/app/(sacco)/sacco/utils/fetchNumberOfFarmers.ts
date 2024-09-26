8349e3e2f68de41ecd18f9cd458df79b7ffe9334
Akwang-Deu <akwangdeu@gmail.com>
2024-09-26 19:14:21 +0300

    add overview

---
+++ fanikisha-next.js/src/app/(sacco)/sacco/utils/fetchNumberOfFarmers.ts
@@ -0,0 +1,37 @@
+export interface Farmers{
+    cooperative_name: string;
+    is_eligible: boolean;
+    farmer_id: number;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    created_at : string;
+    cooperative_number: number;
+    sacco_name: string;
+  
+}
+
+export const fetchFarmers = async () =>{
+    try{
+        const response = await fetch('/api/farmers', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+    if (!response.ok){
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+
+    }
+    return await response.json();
+
+ }  catch (error){
+    throw new Error('Failed to fetch data')
+ }
+};
+
+
+
+
+
--- fanikisha-next.js/src/app/page.tsx
+++ fanikisha-next.js/src/app/page.tsx
@@ -1,11 +1,9 @@
-import Layout from './components/Layout';
+
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <Layout>
-        <div className="flex">
-          <main className="flex-1">{children}</main>
-        </div>
-        </Layout>
+    <div></div>
   );
 }
 
