19f9bf32e64396087592051531a56368536c122b
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-09-27 21:14:31 +0300

    add accounts

--- fanikisha-next.js/.next_temp/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/student/Inganji-Frontend/fanikisha-next.js/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
