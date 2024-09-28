ea7c602fdc1284dce583cd6bf26c4f6b798b74d5
ingabirefanny <fannyingabiree@gmail.com>
2024-09-28 09:41:15 +0300

    Created sidebar

--- src/app/(admin)/admin/components/page.tsx
+++ src/app/(admin)/admin/components/page.tsx
@@ -1,13 +1,13 @@
-// import Layout from "./Layout";
-// import Sidebar from "./Sidebar";
+import Layout from "./Layout";
+import Sidebar from "./Sidebar";
 
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//     return (
-//         <Layout>
-//           <div className="flex">
-//             <main className="flex-1">{children}</main>
-//             <Sidebar/>
-//           </div>
-//           </Layout>
-//     );
-//   }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <Layout>
+          <div className="flex">
+            <main className="flex-1">{children}</main>
+            <Sidebar/>
+          </div>
+          </Layout>
+    );
+  }
