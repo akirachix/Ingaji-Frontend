7340fbc5c8d592c41083c1fd7531252ab9f1d321
Agnes <agnesajema@gmail.com>
2024-10-03 20:21:59 +0300

    resolved errors

--- fanikisha-next.js/src/app/components/Layout/index.tsx
+++ fanikisha-next.js/src/app/components/Layout/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-<<<<<<< HEAD
 // import Sidebar from '../Sidebar'
 
 // import SignUp from '@/app/sign-Up/page';
@@ -9,21 +8,8 @@ import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    
-    <div >
-    {/* <SignUp/> */}
-   
-      
-       
-
-      
-=======
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
     <div>
       {children} 
->>>>>>> 8b24be37c1919f1cc9154a4d048f664f8fe9c902
     </div>
   );
 }
