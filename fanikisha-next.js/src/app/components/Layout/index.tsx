67bf805f59b35217d22fab5a2e4ed12751c452ab
Faith-Hope <wachiraafaith@gmail.com>
2024-10-04 10:42:17 +0300

    Add Cooperatives

--- fanikisha-next.js/src/app/components/Layout/index.tsx
+++ fanikisha-next.js/src/app/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import Sidebar from '../Sidebar';
+import Sidebar from '@/app/(sacco)/sacco/components/Sidebar';
 
 
 export default function Layout({ children, }: { children: React.ReactNode }) {
